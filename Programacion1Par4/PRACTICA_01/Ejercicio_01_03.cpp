52e06bd7e3a341cc7f2ffaa01ea18a204e4804c9
lia.espinoza.ucb <lia.espinoza@ucb.edu.bo>
2026-08-19 15:58:32 -0400

    ejercicio 3

---
+++ Programacion1Par4/PRACTICA_01/Ejercicio_01_03.cpp
@@ -0,0 +1,30 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor:Lia Catalina Espinoza Estrada
+
+// Carrera del estudiante: Ing mecatronica
+
+// Fecha creación: 18/08/2026
+#include <iostream>
+using namespace std;
+
+int main (){
+    int edad = 0;
+    char sexo = 0;
+    double altura = 0;
+
+    cout << "Escribe tu edad:" << endl;
+    cin >> edad;
+
+    cout << "Escribe tu sexo (M/F):" << endl;
+    cin >> sexo;
+
+    cout << "Escribe tu altura en metros:" << endl;
+    cin >> altura;
+
+    cout << "Edad: " << edad << endl;
+    cout << "Sexo: " << sexo << endl;
+    cout << "Altura: " << altura << " metros" << endl;
+
+    return 0;
+}
