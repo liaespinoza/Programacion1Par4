bf35b8d06cbef306f8742d95c350d75c7835f387
lia.espinoza.ucb <lia.espinoza@ucb.edu.bo>
2026-08-26 17:11:18 -0400

    ejercicio e3

---
+++ Programacion1Par4/PRACTICA_03/Ejercicio_03_03.cpp
@@ -0,0 +1,25 @@
+// Materia: Programación I, Paralelo 4
+
+// Autor:Lia Catalina Espinoza Estrada
+
+// Carrera del estudiante: Ing mecatronica
+
+// Fecha creación: 26/08/2026
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int numero=0;
+    int suma=0;
+
+    cout<<"Escriba el numero de elementos: "<< endl;
+     cin >> numero;
+
+     for (int i=1;i<=numero;i++){
+        suma+=i;
+     }
+
+         cout<<"La suma es: "<< suma << endl;
+         return 0;
+    }
